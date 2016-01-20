93d443fc59eb488f49b9021cd428fbae121f1d4c
Rasmus Rahbek <rr@spejdersport.dk>
2016-01-20 08:13:08 +0100

    rettede fejl med dobbelt tekst

--- label_machine.rb
+++ label_machine.rb
@@ -6,10 +6,10 @@ class LabelMachine
   def initialize
     @label_width = 136.062992126
     @label_height = 70.8661417323
-    @label = Prawn::Document.new({page_size: [label_width, label_height], margin: 0})
+    # @label = Prawn::Document.new({page_size: [label_width, label_height], margin: 0})
   end
   def create(options = {})
-    label = @label
+    label = Prawn::Document.new({page_size: [label_width, label_height], margin: 0})
     outputter = Barby::PrawnOutputter.new(options[:barcode])
     outputter.height = 15
     outputter.xdim = 0.7
