2d0ad5d20147bbb6fc9a5c9ba573b0294ebb27ca
Emil Bonne Kristiansen <emilbonnek@gmail.com>
2016-09-12 22:41:00 +0200

    Fjerner funktion der ikke bliver brugt

--- run_server.rb
+++ run_server.rb
@@ -55,7 +55,6 @@ post '/print' do
   end
 end
 
-
 # --
 # ALLE kollonner i datafilen skal være i listen herunder i korrekt rækkefølge.
 headers  = [:bar_num, :description, :item_num, :variant, :price, :x, :x2, :x3, :x4, :x5, :x6, :x7]
@@ -66,17 +65,6 @@ Csv = CSV.read('varer.csv', write_headers: true, headers:headers, encoding: "CP8
 # Fjern kolonner der ikke er i brug
 (headers - relevant).each do |irelevant|
   Csv.delete(irelevant)
-end
-
-def search_textfile(item_number)
-  products = Csv.find_all {|row| row[:item_num].include?(item_number) and row[:bar_num].start_with?("29")}
-  products.map! {|product| product.to_hash}
-  products.each do |product|
-    product[:description].chomp! " - #{product[:variant]}"
-    product[:description].chomp! product[:variant]
-    product[:description] = product[:description].split.join(" ")
-  end
-  return products
 end
 
 get '/' do
