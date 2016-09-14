a91a65952b41e89ea3380141e34d550d6bcfba36
Rasmus Rahbek <rahbekweb@gmail.com>
2016-09-14 17:21:43 +0200

    tilføjet rigtige navne til heders fra csv filen

--- run_server.rb
+++ run_server.rb
@@ -57,9 +57,9 @@ end
 
 # --
 # ALLE kollonner i datafilen skal være i listen herunder i korrekt rækkefølge.
-headers  = [:bar_num, :barcode_description, :item_num, :variant, :price, :description, :x2, :x3, :x4, :x5, :x6, :x7]
+headers  = [:bar_num, :barcode_description, :item_num, :variant, :price, :description, :x2, :l_num, :divison, :seson, :x6, :varant_code]
 # De kollonner der skal bruges skal OGSÅ fremgå i listen herunder.
-relevant = [:bar_num, :item_num, :variant, :price, :description]
+relevant = [:bar_num, :item_num, :variant, :price, :description, :x2, :x6]
 Csv = CSV.read('varer.csv', write_headers: true, headers:headers, encoding: "CP850", col_sep: ';', quote_char: "@")
 
 # Fjern kolonner der ikke er i brug
